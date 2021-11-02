36726b99c327df74275a873b71c9a2790a1215e5
tharcissie <tharcissieidufashe@gmail.com>
2021-11-02 18:10:10 +0200

    variables

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,8 +1,10 @@
+const lat= '1.9'
+const long ='30.06'
 
 const weatherAPI = {
     async fetchMyweather() {
         //const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`
-        const url ="https://fcc-weather-api.glitch.me/api/current?lat=1.9&lon=30.06";
+        const url =`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
         const response = await fetch(url, {
             method: 'GET',
             headers: {
